e687256fc57d76745cf34dcf64b51303fa9d29fe
isakamasayuki <roentgenwinter@gmail.com>
2021-11-08 19:37:09 +0900

    カルーセルに画像を挿入

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,15 @@
     <!-- カルーセルコンポーネント -->
     <v-carousel
       cycle
-      height="400"
+      height="450"
       hide-delimiter-background
       show-arrows-on-hover
     >
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">{{ slide }} Slide</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
     </v-carousel>
     <!-- ボタン -->
     <v-container :class="$style.container">
@@ -87,14 +85,20 @@
 export default {
   data() {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-2',
+      items: [
+        {
+          src: '/R6BP.jpeg',
+        },
+        {
+          src: '/駅.jpeg',
+        },
+        {
+          src: '/市役所.jpeg',
+        },
+        {
+          src: '/シビック.jpeg',
+        },
       ],
-      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
     }
   },
 }
